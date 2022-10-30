a50799bb360340e1f7c4e37fceba08749db9d4b8
PhubestSrikooon <forbf100@gmail.com>
2022-10-30 15:48:59 +0700

    add : anoucement api and element (still have some bug :()

--- js/home.js
+++ js/home.js
@@ -1,3 +1,8 @@
+async function getGameApi(url) {
+  return (await fetch(url)).json();
+}
+
+
 // home banner
 
 
@@ -77,21 +82,15 @@ init()
 // featured banner
 let featuredslideContainer = document.querySelector('.section-featured-slide__container')
 
-async function getGameApi(url) {
-  return (await fetch(url)).json();
-}
-
 async function banner() {
   let data = await getGameApi('https://gamertocoder.garena.co.th/api/minigames')
   let position = 0
   let featuredSlide = ``
   console.log(data)
-  featuredslideContainer.innerHTML = ""
 
 
   for (let i = 0; i < data.length; i++) {
     let imgsrc = null
-    console.log(i)
     if (data[i]['images'] == null){
       imgsrc = 'images/wall-01.jpg'
     }else{
@@ -121,6 +120,7 @@ async function banner() {
   featuredslideContainer.innerHTML = featuredSlide
 
 
+
   function automaticslide(){
     console.log(position)
     let slide = featuredslideContainer.querySelectorAll('.slide')
@@ -136,3 +136,114 @@ async function banner() {
 }
 
 banner()
+
+
+
+// anoucement
+async function anoucement(){
+  let data = [
+    {
+      img:'https://resource.blockmanmobile.com/web/BMGOfficialWebsite/News/Garena/LogoKv.png',
+      type:'news',
+      href:'',
+      name:'ประกาศ Garena',
+      description:'Garena จะเป็นผู้เผยแพร่ Blockman GO และ Blockman GO เวอร์ชั่นใหม่ของ Garena จะเปิดตัวเร็วๆ นี้'
+    },
+    {
+      img:'https://resource.blockmanmobile.com/web/BMGOfficialWebsite/News/Garena/pp2.jpg',
+      type:'news',
+      href:'',
+      name:'Parental FAQ',
+      description:'Garena Blockman GO - ข้อมูลสำหรับผู้ปกครอง'
+    },
+    {
+      img:'https://resource.blockmanmobile.com/web/BMGOfficialWebsite/News/Garena/LogoKv.png',
+      type:'news',
+      href:'',
+      name:'Garena Blockman GO มาตรฐานชุมชน',
+      description:'มาตรฐานชุมชนของเราได้รับการออกแบบมาเพื่อให้แพลตฟอร์มของเราสนุกและปลอดภัยสำหรับทุกคน และในฐานะที่เป็นส่วนหนึ่งของชุมชนของเรา คุณตกลงที่จะปฏิบัติตามมาตรฐานชุมชนเหล่านี้'
+    },
+    {
+      img:'https://resource.blockmanmobile.com/web/BMGOfficialWebsite/News/patchnote/PCOB5-1020X526.jpg',
+      type:'patch',
+      href:'',
+      name:'Blockman GO OB5 Patch Notes',
+      description:"What's news"
+    },
+  ]
+
+  let section_anoucement_content_list = document.querySelector('.section-announcements__content .content-list')
+  let tab = section_anoucement_content_list.querySelector('.content-list__tab')
+  let content = section_anoucement_content_list.querySelector('.content-list__content')
+  let button = tab.querySelectorAll('button')
+
+  let currentTab = 0
+
+  function query(tag){
+    let temp = ``
+    data.forEach((item)=>{
+      if (item.description.length > 100){
+       item.description = `${item.description.slice(0,60)}...`
+      }
+      if (item.type == tag){
+        temp += `
+        <a href="${item.href}">
+        <li>
+          <div class="image-container">
+            <img src="${item.img}" alt="" />
+          </div>
+          <div class="description">
+            <h3>${item.name}</h3>
+            <p>${item.description}</p>
+          </div>
+        </li>
+      </a>
+        `
+      }else if(tag == "all"){
+        temp += `
+        <a href="${item.href}">
+        <li>
+          <div class="image-container">
+            <img src="${item.img}" alt="" />
+          </div>
+          <div class="description">
+            <h3>${item.name}</h3>
+            <p>${item.description}</p>
+          </div>
+        </li>
+      </a>
+        `
+      }
+    })
+    return temp
+  }
+
+  function setColor(index){
+    for (let i=0;i < button.length;i++){
+      button[index].classList.remove('active')
+    }
+    button[index].classList.add('active')
+  }
+  
+
+  button[0].addEventListener('click',()=>{
+    setColor(0)
+    content.innerHTML = query('all')
+  })
+  button[1].addEventListener('click',()=>{
+    setColor(1)
+    content.innerHTML = query('news')
+  })
+  button[2].addEventListener('click',()=>{
+    setColor(2)
+    content.innerHTML = query('event')
+  })
+  button[3].addEventListener('click',()=>{
+    setColor(3)
+    content.innerHTML = query('patch')
+  })
+  setColor(0);
+  content.innerHTML = query('all')
+}
+
+anoucement()
