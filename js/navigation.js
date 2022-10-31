77b6bd0e0218b07f313bcf73089e93d92c5171da
PhubestSrikooon <forbf100@gmail.com>
2022-10-31 20:53:33 +0700

    fix color in jam css

--- js/navigation.js
+++ js/navigation.js
@@ -23,22 +23,22 @@ let toc = [
     [
       [
         '<span class="material-symbols-outlined nav-li-icon">Home</span>',
-        'Home',
+        'หน้าแรก',
         '#main_title',
       ],
       [
         '<span class="material-symbols-outlined nav-li-icon">info</span>',
-        'Featured',
+        'ฟีเจอร์',
         '#featured',
       ],
       [
         '<span class="material-symbols-outlined nav-li-icon">announcement</span>',
-        'Anoucements',
+        'ประกาศ',
         '#announcements',
       ],
       [
         '<span class="material-symbols-outlined nav-li-icon">download</span>',
-        'Download',
+        'ดาวโหลด',
         '#download',
       ],
     ],
@@ -48,12 +48,12 @@ let toc = [
     [
       [
         '<span class="material-symbols-outlined nav-li-icon">Download</span>',
-        'Download',
+        'ดาวโหลด',
         '#download',
       ],
       [
         '<span class="material-symbols-outlined nav-li-icon">devices_other</span>',
-        'Requirement',
+        'ความต้องการ',
         '#requirement',
       ],
     ],
@@ -63,17 +63,17 @@ let toc = [
     [
       [
         '<span class="material-symbols-outlined nav-li-icon">payments</span>',
-        'Prize',
+        'รางวัล',
         '#prize',
       ],
       [
         '<span class="material-symbols-outlined nav-li-icon">info</span>',
-        'Detail',
+        'รายลัเอียด',
         '#detail',
       ],
       [
         '<span class="material-symbols-outlined nav-li-icon">how_to_reg</span>',
-        'Register',
+        'ลงทะเบียน',
         '#register',
       ],
     ],
@@ -87,27 +87,27 @@ let menupath = [
   },
   {
     icon: '<span class="material-symbols-outlined nav-li-icon">Home</span>',
-    text: 'Home',
+    text: 'หน้าแรก',
     href: '/index.html',
   },
   {
     icon: '<span class="material-symbols-outlined nav-li-icon">Download</span>',
-    text: 'Download',
+    text: 'ดาวโหลด',
     href: '/download.html',
   },
   {
     icon: '<span class="material-symbols-outlined nav-li-icon">Forum</span>',
-    text: 'Forum',
+    text: 'ฟอร์รัม',
     href: 'https://blockmanforum.garena.com/',
   },
   {
     icon: '<span class="material-symbols-outlined nav-li-icon">terminal</span>',
-    text: 'Mini-game',
+    text: 'มินิเกม',
     href: '/',
   },
   {
     icon: '<span class="material-symbols-outlined nav-li-icon">phone</span>',
-    text: 'Follow Us',
+    text: 'ติดตาม พวกเรา',
     submenu: [
       {
         icon: '<svg xmlns="http://www.w3.org/2000/svg" style="width:24px; fill:var(--yellow);" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/></svg>',
