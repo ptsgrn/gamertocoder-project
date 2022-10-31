a682072f78a0d6bc9fbfd0b6864a3e9e1230fe42
Patsagorn <49602385+ptsgrn@users.noreply.github.com>
2022-10-31 21:23:37 +0700

    shit

---
+++ js/minigames.js
@@ -0,0 +1,40 @@
+async function init() {
+  document.querySelector('#phd-all-game').innerHTML = await createSuggestion()
+}
+
+async function createSuggestion() {
+  let data = await getAllGames()
+  let ret = ''
+  for (let game of data) {
+    console.log(game)
+    ret += `<a href="/minigame.html?id=${game.no}" class="item">
+            <div class="item__image">
+              <img
+                src="${
+                  game.images === null
+                    ? '/images/banner-01.png'
+                    : game.images[0]
+                }"
+                alt="${game.name} preview images" />
+            </div>
+            <div class="item__content">
+              <img src="${game.icon}" alt="${game.name} logo" />
+              <div class="text">
+                <h3>${game.name}</h3>
+                <p>
+                  ${game.description}
+                </p>
+              </div>
+            </div>
+          </a>`
+  }
+  return ret
+}
+
+async function getAllGames() {
+  return await (
+    await fetch('https://gamertocoder.garena.co.th/api/minigames')
+  ).json()
+}
+
+init()
